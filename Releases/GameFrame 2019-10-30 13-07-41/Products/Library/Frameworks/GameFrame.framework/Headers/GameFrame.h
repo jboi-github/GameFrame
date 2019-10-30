c1ec9f82628f72c2a1bb2379b695980c5b157f4c
jboi-github <jboi.atwork@icloud.com>
2019-10-30 13:10:37 +0100

    Upload new version

---
+++ Releases/GameFrame 2019-10-30 13-07-41/Products/Library/Frameworks/GameFrame.framework/Headers/GameFrame.h
@@ -0,0 +1,19 @@
+//
+//  GameFrame.h
+//  GameFrame
+//
+//  Created by Juergen Boiselle on 29.10.19.
+//  Copyright © 2019 Juergen Boiselle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GameFrame.
+FOUNDATION_EXPORT double GameFrameVersionNumber;
+
+//! Project version string for GameFrame.
+FOUNDATION_EXPORT const unsigned char GameFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameFrame/PublicHeader.h>
+
+
